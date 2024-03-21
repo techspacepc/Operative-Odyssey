42175e9868cefcaeeebe2ddb9b7adf544d0b7774
Wumpie7 <christan.versteeg@hva.nl>
2024-03-21 15:33:55 +0100

    Added base collider generation for snapping

---
+++ Assets/Scripts/OrganSnappingCollider.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class OrganSnappingCollider : MonoBehaviour
+{
+    void Start()
+    {
+        bool isNewCollider = false;
+
+        if (!gameObject.TryGetComponent(out SphereCollider sphere))
+        {
+            sphere = gameObject.AddComponent<SphereCollider>();
+            isNewCollider = true;
+        }
+
+        SphereCollider snappingPoint = new GameObject("SnappingPoint", typeof(SphereCollider)).GetComponent<SphereCollider>();
+
+        snappingPoint.radius = sphere.radius;
+        snappingPoint.center = sphere.center;
+
+        if (isNewCollider)
+            Destroy(sphere);
+
+        gameObject.transform.SetParent(snappingPoint.transform);
+    }
+}
