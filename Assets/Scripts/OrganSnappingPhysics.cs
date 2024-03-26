41e660a00c4cc38f7b193d89e3976f679cbadd52
Wumpie7 <christan.versteeg@hva.nl>
2024-03-26 18:04:59 +0100

    Updated the physics to use selectEntered callback

--- Assets/Scripts/OrganSnappingPhysics.cs
+++ Assets/Scripts/OrganSnappingPhysics.cs
@@ -1,22 +1,33 @@
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class OrganSnappingPhysics : MonoBehaviour
 {
     private Transform child;
     private new Rigidbody rigidbody;
 
-    private void Start()
+    [SerializeField] private XRGrabInteractable interactable;
+
+    public void OnSelectEnter(SelectEnterEventArgs args)
     {
-        child = transform.GetChild(0);
-        rigidbody = child.GetComponent<Rigidbody>();
+        Debug.Log("Callback");   
     }
 
-    private void OnCollisionEnter(Collision collision)
+    private void Start()
     {
-        if (collision.transform == child)
-        {
-            child.position = transform.position;
-            rigidbody.isKinematic = true;
-        }
+        //interactable = GetComponent<XRGrabInteractable>();
+
+        interactable.selectEntered.AddListener(OnSelectEnter);
+        //child = transform.GetChild(0);
+        //rigidbody.isKinematic = true;
     }
+
+    /*    private void OnCollisionEnter(Collision collision)
+        {
+            if (collision.transform == child)
+            {
+                child.position = transform.position;
+                //rigidbody.isKinematic = true;
+            }
+        }*/
 }
