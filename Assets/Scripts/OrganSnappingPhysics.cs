c6eb481d51839a60e13b17b0e669cdbc65222d7e
Wumpie7 <chrisjaver@gmail.com>
2024-03-27 14:39:03 +0100

    Organs can now be socketed into the torso
    Also removed the OrganSnappingCollider, since the XR Socket Interactor does everything I need to do. (Making that aformentioned script redundant).

--- Assets/Scripts/OrganSnappingPhysics.cs
+++ Assets/Scripts/OrganSnappingPhysics.cs
@@ -1,33 +1,47 @@
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 
+/// <summary>
+/// Yes this class does not do anything right now, turns out the things I needed were presets in the XR environment that I can just use.
+/// However this might be used in the future for freezing objects in space - if I learn how to get specific button presses into an if statement.
+/// For now this class just serves as an example on how to run events with interactables. (Hence I'm not removing it).
+/// </summary>
 public class OrganSnappingPhysics : MonoBehaviour
 {
-    private Transform child;
-    private new Rigidbody rigidbody;
+    private Rigidbody rb;
+    private XRGrabInteractable interactable;
 
-    [SerializeField] private XRGrabInteractable interactable;
-
-    public void OnSelectEnter(SelectEnterEventArgs args)
+    public void OnSelectEnter(SelectEnterEventArgs _)
     {
-        Debug.Log("Callback");   
+        Debug.Log(nameof(OnSelectEnter));
+    }
+
+    public void OnSelectExit(SelectExitEventArgs _)
+    {
+        Debug.Log(nameof(OnSelectExit));
+
+        //rb.isKinematic = false;
+    }
+
+    private void Awake()
+    {
+        interactable = GetComponent<XRGrabInteractable>();
+    }
+
+    private void OnEnable()
+    {
+        interactable.selectEntered.AddListener(OnSelectEnter);
+        interactable.selectExited.AddListener(OnSelectExit);
     }
 
     private void Start()
     {
-        //interactable = GetComponent<XRGrabInteractable>();
-
-        interactable.selectEntered.AddListener(OnSelectEnter);
-        //child = transform.GetChild(0);
-        //rigidbody.isKinematic = true;
+        rb = GetComponent<Rigidbody>();
     }
 
-    /*    private void OnCollisionEnter(Collision collision)
-        {
-            if (collision.transform == child)
-            {
-                child.position = transform.position;
-                //rigidbody.isKinematic = true;
-            }
-        }*/
+    private void OnDisable()
+    {
+        interactable.selectEntered.RemoveListener(OnSelectEnter);
+        interactable.selectExited.AddListener(OnSelectExit);
+    }
 }
