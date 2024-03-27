b8020b770878668e50cc7ca3b6e169e0194efd5f
Wumpie7 <chrisjaver@gmail.com>
2024-03-27 20:31:40 +0100

    Simplified ResetInteractable, added tray snapping

--- Assets/Scripts/ResetInteractable.cs
+++ Assets/Scripts/ResetInteractable.cs
@@ -5,18 +5,10 @@ public class ResetInteractable : MonoBehaviour
 {
     private XRSocketInteractor socket;
 
-    private XRInteractionManager manager;
-
     private void ResetObject()
-        => manager.SelectEnter(socket as IXRSelectInteractor, socket.startingSelectedInteractable);
+        => socket.interactionManager.SelectEnter(socket as IXRSelectInteractor, socket.startingSelectedInteractable);
 
-    private void Awake()
-    {
-        socket = GetComponent<XRSocketInteractor>();
-
-        // This should really be a singleton in scene, so it can be statically accessed.
-        manager = FindObjectOfType<XRInteractionManager>();
-    }
+    private void Awake() => socket = GetComponent<XRSocketInteractor>();
 
     private void OnEnable() => ResetButton.OnReset += ResetObject;
 
