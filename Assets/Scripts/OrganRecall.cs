743146f19b120b791aaa19247eb25218af565112
Wumpie7 <chrisjaver@gmail.com>
2024-04-12 08:07:04 +0200

    Added camera and timer check for organ recall

--- Assets/Scripts/OrganRecall.cs
+++ Assets/Scripts/OrganRecall.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 
@@ -10,9 +11,39 @@ namespace Organs
     {
         private XRSocketInteractor socket;
 
+        private Coroutine recallCountdown;
+        private readonly WaitForSeconds countdown = new(3);
+
         private void Recall()
             => socket.interactionManager.SelectEnter(socket as IXRSelectInteractor, socket.startingSelectedInteractable);
 
+        private IEnumerator RecallCoroutine()
+        {
+            yield return countdown;
+
+            Recall();
+        }
+
         private void Awake() => socket = GetComponent<XRSocketInteractor>();
+
+        private void OnTriggerExit(Collider other)
+        {
+            // TODO: Implement bounding box check for when organ exits the playing field.
+        }
+
+        private void OnBecameVisible()
+        {
+            if (recallCountdown != null) return;
+
+            recallCountdown = StartCoroutine(RecallCoroutine());
+        }
+
+        private void OnBecameInvisible()
+        {
+            if (recallCountdown == null) return;
+
+            StopCoroutine(recallCountdown);
+            recallCountdown = null;
+        }
     }
 }
