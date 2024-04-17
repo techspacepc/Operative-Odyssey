0d81177bf426a2d965afe7cb2a4bef6abff2a61d
Jordi Prins <103563010+JordinatorP@users.noreply.github.com>
2024-04-17 11:06:46 +0200

    Basic visibility fixed

---
+++ Assets/Scripts/VisibilityManager.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class VisibilityManager : MonoBehaviour
+{
+    public GameObject Tray;
+    public GameObject Torso;
+    public GameObject Knife;
+
+    private XRSocketInteractor socket;
+
+    private void Awake()
+    {
+        socket = Tray.GetComponent<XRSocketInteractor>();
+        socket.selectEntered.AddListener(OnObjectInserted);
+        socket.selectExited.AddListener(OnObjectRemoved);
+    }
+
+    private void OnDisable()
+    {
+        socket.selectEntered.RemoveListener(OnObjectInserted);
+        socket.selectExited.RemoveListener(OnObjectRemoved);
+    }
+
+    private void Start()
+    {
+        Knife.SetActive(false);
+    }
+
+    private void OnObjectInserted(SelectEnterEventArgs args)
+    {
+        Torso.SetActive(false);
+        Knife.SetActive(true);
+    }
+
+    private void OnObjectRemoved(SelectExitEventArgs args)
+    {
+        Torso.SetActive(true);
+        Knife.SetActive(false);
+    }
+}
