570287dea90791136641f54743d3662149380143
DaftFoxx <94451887+DaftFoxx@users.noreply.github.com>
2024-04-17 12:26:56 +0200

    Hitboxes and Prefab
    Added hitboxes for the kidneys and created a prefab for the cut kidney. Also emptied the previous OrganDissect script to rewrite it.

--- Assets/Scripts/OrganDissect.cs
+++ Assets/Scripts/OrganDissect.cs
@@ -4,14 +4,11 @@ using UnityEngine;
 
 public class OrganDissect : MonoBehaviour
 {
-    [SerializeField] private Transform left, right;
-    [SerializeField] private Vector3 offset;
 
     // Start is called before the first frame update
     private void Start()
     {
-        left.position = transform.position + offset;
-        right.position = transform.position - offset;
+
     }
 
     // Update is called once per frame
