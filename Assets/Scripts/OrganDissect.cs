69f0516bddf23356df80614370c7ec04c47235f1
DaftFoxx <94451887+DaftFoxx@users.noreply.github.com>
2024-04-17 16:50:03 +0200

    Asymmetrical cutting added
    You can now cut an asymmetrical organ

--- Assets/Scripts/OrganDissect.cs
+++ Assets/Scripts/OrganDissect.cs
@@ -9,8 +9,18 @@ public class OrganDissect : MonoBehaviour
     private Vector3 enterPoint, exitPoint;
 
     [Header("Symmetrical Dissection")]
-    [SerializeField] private bool isSymmetrical;
+    [SerializeField] private bool cutSymmetrical;
     [SerializeField] private GameObject half;
+
+    [System.Serializable]
+    public class Data
+    {
+        public GameObject obj;
+        public Vector3 loc;
+    }
+
+    [Header("Asymmetrical Dissection")]
+    [SerializeField] public Data[] parts;
 
     private void OnTriggerEnter(Collider other)
     {
@@ -32,10 +42,20 @@ public class OrganDissect : MonoBehaviour
 
     private void Dissect()
     {
-        GameObject leftHalf = Instantiate(half, transform.position, transform.rotation);
-        GameObject rightHalf = Instantiate(half, transform.position, transform.rotation);
+        if (cutSymmetrical)
+        {
+            GameObject leftHalf = Instantiate(half, transform.position, transform.rotation);
+            GameObject rightHalf = Instantiate(half, transform.position, transform.rotation);
 
-        rightHalf.transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y, transform.localScale.z * -1);
+            rightHalf.transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y, transform.localScale.z * -1);
+        }
+        else
+        {
+            for (int i = 0; i < parts.Length; i++)
+            {
+                Instantiate(parts[i].obj, transform.position + parts[i].loc, transform.rotation);
+            }
+        }
 
         Destroy(gameObject);
     }
