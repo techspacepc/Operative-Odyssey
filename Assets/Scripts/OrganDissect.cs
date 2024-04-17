36aa9236df4ccaf6f714685f468f4a591e9df468
DaftFoxx <94451887+DaftFoxx@users.noreply.github.com>
2024-04-17 16:21:57 +0200

    Dissecting symmetrically works
    You can now cut organs symmetrically and only when the organ is not in your hand

--- Assets/Scripts/OrganDissect.cs
+++ Assets/Scripts/OrganDissect.cs
@@ -5,12 +5,16 @@ using UnityEngine;
 public class OrganDissect : MonoBehaviour
 {
     private bool dissecting;
+    private bool grabbed;
     private Vector3 enterPoint, exitPoint;
+
+    [Header("Symmetrical Dissection")]
+    [SerializeField] private bool isSymmetrical;
     [SerializeField] private GameObject half;
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Scalpel"))
+        if (other.CompareTag("Scalpel") && !grabbed)
         {
             dissecting = true;
             enterPoint = transform.position - other.transform.position;
@@ -19,14 +23,14 @@ public class OrganDissect : MonoBehaviour
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.CompareTag("Scalpel") && dissecting)
+        if (other.CompareTag("Scalpel") && dissecting && !grabbed)
         {
-            dissect();
+            Dissect();
             exitPoint = transform.position - other.transform.position;
         }
     }
 
-    private void dissect()
+    private void Dissect()
     {
         GameObject leftHalf = Instantiate(half, transform.position, transform.rotation);
         GameObject rightHalf = Instantiate(half, transform.position, transform.rotation);
@@ -34,5 +38,16 @@ public class OrganDissect : MonoBehaviour
         rightHalf.transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y, transform.localScale.z * -1);
 
         Destroy(gameObject);
+    }
+
+    public void Grabbed()
+    {
+        dissecting = false;
+        grabbed = true;
+    }
+
+    public void Released()
+    {
+        grabbed = false;
     }
 }
