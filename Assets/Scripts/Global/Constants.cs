26b14aa8482943b4d38f326cba730162c857f7bb
Wumpie7 <christan.versteeg@hva.nl>
2024-04-23 10:46:48 +0200

    Soft reset cherry pick from fca7dd7

---
+++ Assets/Scripts/Global/Constants.cs
@@ -0,0 +1,21 @@
+namespace Constants
+{
+    public static class Tag
+    {
+        public static readonly string Untagged = nameof(Untagged);
+        public static readonly string Respawn = nameof(Respawn);
+        public static readonly string Finish = nameof(Finish);
+        public static readonly string EditorOnly = nameof(EditorOnly);
+        public static readonly string MainCamera = nameof(MainCamera);
+        public static readonly string Player = nameof(Player);
+        public static readonly string GameController = nameof(GameController);
+        public static readonly string Anchor = nameof(Anchor);
+        public static readonly string Scalpel = nameof(Scalpel);
+        public static readonly string Tray = nameof(Tray);
+    }
+
+    public static class Const
+    {
+        // For future use.
+    }
+}
