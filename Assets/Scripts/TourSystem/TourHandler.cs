013e3b2e3fbc406d8b8bda7f43425db4058180b5
Wumpie7 <chrisjaver@gmail.com>
2024-05-23 01:21:21 +0200

    Reworked TourHandler
    There's a bug. It might be on line 45 as an order of execution issue.

--- Assets/Scripts/TourSystem/TourHandler.cs
+++ Assets/Scripts/TourSystem/TourHandler.cs
@@ -1,48 +1,88 @@
-using System.Collections;
-using System.Collections.Generic;
+using Tags;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit.Filtering;
 
 public class TourHandler : MonoBehaviour
 {
-    [SerializeField] private GameObject heartTour;
-    [SerializeField] private GameObject kidneyTour;
-    [SerializeField] private GameObject eyeTour;
-    [SerializeField] private GameObject museum;
-    [SerializeField] private GameObject player;
-    private Collider colliderGate;
-    private Vector3 newPosition = new Vector3(0, 0, 0);
+    [SerializeField] private GameObject museumParent;
+    [SerializeField] private GameObject[] tourParents;
 
-    private void Start(){
-        // Check if the GameObject has a Collider component
-        colliderGate = GetComponent<Collider>();
+    private XRSimpleInteractable[] tourExitButtons;
+
+    private Transform playerTransform;
+
+    private Vector3 resetPosition = new(1, 0, 1);
+
+    private int currentTour;
+
+    private void StartTour()
+    {
+        DelegatePortalCollision.OnPortalEntered -= StartTour;
+
+        museumParent.SetActive(false);
+        tourParents[currentTour].SetActive(true);
+
+        DelegatePortalCollision.OnPortalEntered += ContinueTour;
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void ContinueTour()
     {
-        if (other.CompareTag("Player"))
+        tourParents[currentTour++].SetActive(false);
+        tourParents[currentTour].SetActive(true);
+    }
+
+    private void ExitTour(SelectEnterEventArgs _)
+    {
+        DelegatePortalCollision.OnPortalEntered -= ContinueTour;
+
+        foreach (GameObject tour in tourParents)
+            tour.SetActive(false);
+
+        museumParent.SetActive(true);
+
+        playerTransform.position = resetPosition;
+
+        currentTour = default;
+
+        DelegatePortalCollision.OnPortalEntered += StartTour;
+    }
+
+    private void Awake()
+    {
+        playerTransform = GameObject.FindGameObjectWithTag(Tag.Player).GetComponent<Transform>();
+
+        museumParent.GetComponentInChildren<SphereCollider>().AddComponent<DelegatePortalCollision>();
+
+        tourExitButtons = new XRSimpleInteractable[tourParents.Length];
+
+        for (int i = 0; i < tourParents.Length; i++)
         {
-            heartTour.SetActive(true);
-            museum.SetActive(false);
+            GameObject tour = tourParents[i];
 
-            colliderGate.enabled = false;
+            tourExitButtons[i] = tour.GetComponentInChildren<XRPokeFilter>().GetComponent<XRSimpleInteractable>();
+
+            if (i == tourParents.Length - 1) return;
+
+            tour.GetComponentInChildren<SphereCollider>().AddComponent<DelegatePortalCollision>();
         }
-
-        Debug.Log("ENTERING");
     }
 
-    public void EndTour()
+    private void OnEnable()
     {
-        heartTour.SetActive(false);
-        kidneyTour.SetActive(false);
-        eyeTour.SetActive(false);
-        museum.SetActive(true);
+        DelegatePortalCollision.OnPortalEntered += StartTour;
 
-        player.transform.position = newPosition;
-
-        Invoke(nameof(EnableCollider), 0.1f);
+        foreach (XRSimpleInteractable interactable in tourExitButtons)
+            interactable.selectEntered.AddListener(ExitTour);
     }
 
-    private void EnableCollider(){
-        colliderGate.enabled = true;
+    private void OnDisable()
+    {
+        DelegatePortalCollision.OnPortalEntered -= StartTour;
+        DelegatePortalCollision.OnPortalEntered -= ContinueTour;
+
+        foreach (XRSimpleInteractable interactable in tourExitButtons)
+            interactable.selectEntered.RemoveListener(ExitTour);
     }
 }
