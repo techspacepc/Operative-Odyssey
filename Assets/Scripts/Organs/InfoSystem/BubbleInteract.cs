d063a93ef2d686bf5a88414a9bbedcae48557f6d
Wumpie7 <chrisjaver@gmail.com>
2024-06-13 02:39:41 +0200

    Revert "Merge branch 'feature/lazy-tooltips' into 'dev'"
    This reverts commit 29c8148e063d1ba1f345450716496631a3658de6, reversing
    changes made to 6134a5f0c3fa3f39534aec25397bca7793bcf8e3.

--- Assets/Scripts/Organs/InfoSystem/BubbleInteract.cs
+++ Assets/Scripts/Organs/InfoSystem/BubbleInteract.cs
@@ -1,41 +1,39 @@
 using System;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.XR.Interaction.Toolkit;
+using System.Collections.Generic;
 
 public class BubbleInteract : MonoBehaviour
 {
-    public static event Action<string> OnBubbleInteract;
     private GameObject[] allBubbles;
     [SerializeField] private TextMeshPro signText;
     private new Renderer renderer;
 
     private Material defaultMaterial;
     [SerializeField] private Material selectedMaterial;
-
-    [SerializeField] private GameObject textObject;  // Serialized field for text object
-    [SerializeField] private GameObject textMeshPro;  // Serialized field for TextMeshPro component
+    
+    private GameObject textObject;
 
     private XRSimpleInteractable interactable;
 
     private void BubbleInteracted(SelectEnterEventArgs _)
     {
-        // Iterate through all bubbles to check whether they are activated, set unactive if so
+        //iterate through all bubbles to check whether they are activated, set unactive if so
         for (int i = 0; i < allBubbles.Length; i++)
         {
-            BubbleInteract bubbleInteract = allBubbles[i].GetComponent<BubbleInteract>();
-            GameObject text = bubbleInteract.textObject;
+            Transform textTransform = allBubbles[i].transform.GetChild(0);
+            GameObject text = textTransform.gameObject;
 
-            // Check if the text is active
-            if (text.activeSelf)
-            {
-                // Check if the bubble is the interacted bubble
-                if (allBubbles[i].name != name)
-                {
-                    // Disable the text
+            //check if the text is active
+            if(text.activeSelf){
+                //check if the bubble is the interactedbubble
+                if(allBubbles[i].name != name){
+                    //disable the text
                     text.SetActive(false);
 
-                    // Change to default material
+                    //change to default material
                     allBubbles[i].GetComponent<Renderer>().material = defaultMaterial;
                 }
             }else{
@@ -43,18 +41,17 @@ public class BubbleInteract : MonoBehaviour
             }
         }
 
-        if (textObject.activeSelf)
-        {
-            // Deselect the bubble
+        if (textObject.activeSelf){
+            //deselect the bubble
             renderer.material = defaultMaterial;
-            // OnBubbleInteract(name);
             textObject.SetActive(false);
-        }
-        else
-        {
-            // Select the bubble
+
+            //disable text next to bubble
+            TextMeshPro text = textObject.GetComponent<TextMeshPro>();
+            text.enabled = false;
+        }else{
+            //select the bubble
             renderer.material = selectedMaterial;
-            OnBubbleInteract?.Invoke(name);
             textObject.SetActive(true);
 
             //disable text next to bubble
@@ -71,32 +68,22 @@ public class BubbleInteract : MonoBehaviour
 
         defaultMaterial = renderer.material;
 
-        // Check if textObject and textMeshPro are assigned in the Inspector
-        if (textObject == null)
-        {
-            Debug.LogError("Text Object is not assigned in the inspector for " + name);
-            return;  // Exit early to avoid null reference exceptions
-        }
+        // Get the child textobject by index
+        textObject = transform.GetChild(0).gameObject;
 
-        if (textMeshPro == null)
-        {
-            Debug.LogError("TextMeshPro component is not assigned in the inspector for " + name);
-            return;  // Exit early to avoid null reference exceptions
-        }
+        //set name text
+        TextMeshPro text = textObject.GetComponent<TextMeshPro>();
+        text.text = name;
 
-        // Set name text
-        TextMeshProUGUI TextMeshProText = textMeshPro.GetComponent<TextMeshProUGUI>();
-        TextMeshProText.text = name;
-
-        // Set position of the text on the bubble
-        RectTransform textRectTransform = textMeshPro.GetComponent<RectTransform>();
+        //set position of the text on the bubble
+        RectTransform textRectTransform = textObject.GetComponent<RectTransform>();
         textRectTransform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
     }
 
-    private void Start()
-    {
-        // Get all bubbles
-        allBubbles = BubbleHandler.bubbleArray;
+    private void Start(){
+        //get all bubbles
+        BubbleHandler bubbleHandler = transform.parent.GetComponent<BubbleHandler>();
+        allBubbles = bubbleHandler.bubbleArray;
     }
 
     private void OnEnable()
